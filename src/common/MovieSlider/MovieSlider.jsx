4e4bc3343b5234d0f298e177844238cb23aec58a
Young <rhsiddlsidhd@nate.com>
2024-04-10 18:07:38 +0900

    deploy 수정

--- src/common/MovieSlider/MovieSlider.jsx
+++ src/common/MovieSlider/MovieSlider.jsx
@@ -1,18 +1,10 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieSlider.style.css";
 
 const MovieSlider = ({ title, movies, responsive }) => {
-  // const [centerMode, setCenterMode] = useState(true);
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     setCenterMode(window.innerWidth <= 767 ? false : true);
-  //   };
-  //   window.addEventListener("resize", handleResize);
-  // }, []);
-
   return (
     <div>
       <h2
